5da0c7153fdf5dc970b9c15ef2885855c689ace4
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-07-11 12:15:45 -0400

    feat: create choice buttons

---
+++ src/constants/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  'yellow-to': 'hsl(39, 89%, 43%)',
+  'yellow-from': 'hsl(40, 84%, 53%)',
+  'blue-to': 'hsl(230, 80%, 59%)',
+  'blue-from': 'hsl(230, 89%, 65%)',
+  'red-to': 'hsl(349, 60%, 49%)',
+  'red-from': 'hsl(349, 70%, 56%)',
+  'purple-to': 'hsl(261, 68%, 57%)',
+  'purple-from': 'hsl(261, 72%, 63%)',
+  'cyan-to': 'hsl(189, 59%, 45%)',
+  'cyan-from': 'hsl(189, 58%, 57%)',
+  'dark-text': 'hsl(229, 25%, 31%)',
+  'score-text': 'hsl(229, 64%, 46%)',
+  'header-outline': 'hsl(217, 16%, 45%)',
+  'radial-gradient-from': 'hsl(214, 47%, 23%)',
+  'radial-gradient-to': 'hsl(237, 49%, 15%)',
+};
