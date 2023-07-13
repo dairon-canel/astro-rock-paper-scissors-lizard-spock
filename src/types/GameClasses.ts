85a8d1f56025799dac1f7a9b2041825a9a1b9218
Dairon Canel <98697567+dairon-canel@users.noreply.github.com>
2023-07-13 13:08:01 -0400

    feat: add game classes

---
+++ src/types/GameClasses.ts
@@ -0,0 +1,96 @@
+class Action {
+  constructor(
+    private name: string,
+    private defeats: string[],
+    private defeatedBy: string[],
+  ) {}
+
+  getName(): string {
+    return this.name;
+  }
+
+  defeatsAction(action: Action) {
+    return this.defeats.includes(action.name);
+  }
+
+  isDefeatedByAction(action: Action) {
+    return this.defeatedBy.includes(action.name);
+  }
+}
+
+class Player {
+  constructor(
+    private name: string,
+    private selectedAction: Action,
+    private score: number = 0,
+  ) {}
+
+  getName() {
+    return this.name;
+  }
+
+  getSelectedAction() {
+    return this.selectedAction;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  chooseAction(action: Action) {
+    this.selectedAction = action;
+  }
+
+  increaseScore() {
+    this.score++;
+  }
+}
+
+class Game {
+  constructor(
+    private player1: Player,
+    private player2: Player,
+    private actions: Action[],
+  ) {}
+
+  playRound() {
+    this.player1.chooseAction(this.getRandomAction());
+    this.player2.chooseAction(this.getRandomAction());
+
+    this.determineWinner();
+  }
+
+  determineWinner() {
+    const action1 = this.player1.getSelectedAction();
+    const action2 = this.player2.getSelectedAction();
+
+    if (action1 === action2) {
+      console.log("It's a tie!");
+      return;
+    }
+
+    if (action1.defeatsAction(action2)) {
+      console.log(`${this.player1.getName()} wins!`);
+      this.player1.increaseScore();
+    } else {
+      console.log(`${this.player2.getName()} wins!`);
+      this.player2.increaseScore();
+    }
+  }
+
+  getRandomAction() {
+    const randomIndex = Math.floor(Math.random() * this.actions.length);
+    return this.actions[randomIndex];
+  }
+}
+
+// Usage example
+// const rock = new Action('rock', ['scissors'], ['paper']);
+// const paper = new Action('paper', ['rock'], ['scissors']);
+// const scissors = new Action('scissors', ['paper'], ['rock']);
+
+// const player1 = new Player('Player 1', rock);
+// const player2 = new Player('Player 2', paper);
+// const game = new Game(player1, player2, [rock, paper, scissors]);
+
+// game.playRound();
